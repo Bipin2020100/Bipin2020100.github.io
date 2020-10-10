1d94f622de3805b1c84bb8d060a1a2fc262a0b3d
Bipin2020100 <b.regmi2014@gmail.com>
2020-10-10 16:37:05 -0500

    Update assignment.js

--- Assignments/w3d6/assignment.js
+++ Assignments/w3d6/assignment.js
@@ -114,3 +114,6 @@ rabbit.sayHi();// Rabbit
 Rabbit.prototype.sayHi();//undefined
 Object.getPrototypeOf(rabbit).sayHi();//undefined
  rabbit.__proto__.sayHi(); //undefined
+
+ ///////////////////////////////////////////////////////////////////
+
