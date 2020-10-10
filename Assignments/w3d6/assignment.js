ed0154dff43f05105a6fa2a59d965c218f53180e
Bipin2020100 <b.regmi2014@gmail.com>
2020-10-10 17:53:21 -0500

    Update 10/10/2020

--- Assignments/w3d6/assignment.js
+++ Assignments/w3d6/assignment.js
@@ -117,34 +117,4 @@ if (typeof prompt === "undefined") var prompt = require("prompt-sync")();
 
  ///////////////////////////////////////////////////////////////////
 
-//  Create an object called Teacher derived from the Person class, and implement a method called teach
-//  which receives a string called subject, and prints out:
-//  [teacher's name] is now teaching [subject]
-//  Here is code for Person and an example of a Student function constructor. Write Mocha tests that verify
-//  the describe, learn, and teach methods work as expected for students and teachers.
- var Person = function() {};
- Person.prototype.initialize = function(name, age)
- {
-  this.name = name;
-  this.age = age;
- }
- Person.prototype.describe = function()
- {
-  return this.name + ", " + this.age + " years old.";
- }
- var Student = function() {};
- Student.prototype = new Person();
- Student.prototype.learn = function(subject)
- {
-  console.log(this.name + " just learned " + subject);
- }
- var me = new Student();
- me.initialize("John", 25);
- me.learn("Inheritance");
-
-let Teacher = {
-  
-  }
-  __proto__ = 
-}
 
---
+++ Assignments/w3d6/extra.js
@@ -0,0 +1,47 @@
+
+
+/*eslint-disable */
+
+var log = console.log; //shortcut for console.log
+if (typeof alert === "undefined") var alert = console.log; //this will log in node and alert in browser
+if (typeof prompt === "undefined") var prompt = require("prompt-sync")();
+
+
+// Create an object called Teacher derived from the Person class, and implement a method called teach
+//  which receives a string called subject, and prints out:
+//  [teacher's name] is now teaching [subject]
+//  Here is code for Person and an example of a Student function constructor. Write Mocha tests that verify
+//  the describe, learn, and teach methods work as expected for students and teachers.
+ var Person = function() {};
+ Person.prototype.initialize = function(name, age)
+ {
+  this.name = name;
+  this.age = age;
+ }
+ Person.prototype.describe = function()
+ {
+  return this.name + ", " + this.age + " years old.";
+ }
+ var Student = function() {};
+ Student.prototype = new Person();
+ Student.prototype.learn = function(subject)
+ {
+  console.log(this.name + " just learned " + subject);
+ }
+ var me = new Student();
+ me.initialize("John", 25);
+
+ me.learn("Inheritance");
+
+ 
+ let Teacher = function() {};
+ Teacher.prototype = new Person();
+ Teacher.prototype.teach = function(subject)
+ {
+  console.log(this.name + " is teaching " + subject);
+ }
+
+let he = new Teacher();
+he.initialize("Bipin", 30);
+
+he.teach("Inheritance");
